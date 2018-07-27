9f5f51f57673b4c2f54553457e38e5e256373ae9
Corwien <corwien@126.com>
2018-07-27 23:03:57 +0800

    添加备案信息

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -13,26 +13,21 @@
               </div>
               <br>
               <span style="font-size:0.9em">
-                  Powered by <a href="https://github.com/summerblue/phphub5" target="_blank" style="color:inherit">PHPHub</a>
-              </span><br>
+                  Powered by <a href="https://github.com/summerblue/phphub5" target="_blank" style="color:inherit">Digtime</a>
+              </span>&nbsp;
               <span style="font-size:0.9em">
                   Designed by <span style="color: #e27575;font-size: 14px;">❤</span> <a href="https://github.com/summerblue" target="_blank" style="color:inherit">Summer</a>
               </span>
+            <span style="font-size:1.0em;margin-left: 50px;">
+              <a href="http://www.miibeian.gov.cn/" target="_blank">粤ICP备15053415号-1</a>
+            </span>
+
+
           </div>
 
           <div class="col-sm-6 col-lg-6 col-lg-offset-1">
 
               <div class="row">
-                <div class="col-sm-4">
-                  <h4>赞助商</h4>
-                  <ul class="list-unstyled">
-                      @if(isset($banners['footer-sponsor']))
-                          @foreach($banners['footer-sponsor'] as $banner)
-                              <a href="{{ $banner->link }}" target="_blank"><img src="{{ $banner->image_url }}" class="popover-with-html footer-sponsor-link" width="98" data-placement="top" data-content="{{ $banner->title }}"></a>
-                          @endforeach
-                      @endif
-                  </ul>
-                </div>
 
                   <div class="col-sm-4">
                     <h4>{{ lang('Site Status') }}</h4>
@@ -42,15 +37,7 @@
                         <li>{{ lang('Total Reply') }}: {{ $siteStat->reply_count }} </li>
                     </ul>
                   </div>
-                  <div class="col-sm-4">
-                    <h4>其他信息</h4>
-                    <ul class="list-unstyled">
-                        <li><a href="{{ route('sites.index') }}"><i class="fa fa-globe text-md"></i> 推荐网站</a></li>
-                        <li><a href="/about"><i class="fa fa-info-circle" aria-hidden="true"></i> 关于我们</a></li>
-                        <li><a href="{{ route('hall_of_fames') }}"><i class="fa fa-star" aria-hidden="true"></i> {{ lang('Hall of Fame') }}</a></li>
-                        <li><a href="http://yousails.mikecrm.com/4Dh5uWU"><i class="fa fa-thumbs-up" aria-hidden="true"></i> 软件外包服务</a></li>
-                    </ul>
-                  </div>
+
 
                 </div>
 
